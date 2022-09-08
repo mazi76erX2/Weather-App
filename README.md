22b7a961a042e23eea5e09d879fa487120e1ed48
Xolani Mazibuko <mazi76erx@gmail.com>
2022-09-08 19:37:15 +0200

    Added unit tests skelotwn

--- README.md
+++ README.md
@@ -22,8 +22,24 @@ for Windows
 pip install -r requirements.txt
 ```
 
-* Run Django on your local server
+* Run Django on your local server.
 
 ```bash
+python manage.py collectstatic
+python manage.py migrate
 python manage.py runserver
+```
+
+## Testing
+
+1. Before running tests, make sure you have installed the dependencies. (These are included in the requirements file)
+
+```bash
+pip install -r requirements.txt
+```
+
+2. Run a coverage tp check the test coverage
+
+```bash
+python manage.py test
 ```
--- locations/tests.py
+++ locations/tests.py
@@ -1,3 +1,32 @@
+from datetime import datetime, timedelta
 from django.test import TestCase
+from .models import LocationWeather
 
-# Create your tests here.
+
+class LocationWeatherTestCase(TestCase):
+    def setUp(self):
+        self.location_weather = LocationWeather.objects.create(
+            location="Johannesburg",
+            number_of_days=5,
+            start_date=datetime.now(),
+            end_date=datetime.now() + timedelta(days=int(5)),
+            maximum=30.1,
+            minimum=20.1,
+            average=25.1,
+            median=26.1,
+        )
+
+    def test_number_of_days_less_than_1_gives_error(self):
+        pass
+
+    def test_number_of_days_more_than_14_gives_error(self):
+        pass
+
+    def test_api_returns_status_code_200_for_5_days(self):
+        pass
+
+    def test_api_returns_data_for_5_days(self):
+        pass
+
+    def test_api_returns_error_for_unknown_location(self):
+        pass
